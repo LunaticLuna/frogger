d008e3fc57f7919b4bb17334e4843649b53887c2
crazyLuna <lijiemessage@gmail.com>
2018-08-21 23:18:25 -0400

    enemy and player can move, will freeze after collision

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 // Enemies our player must avoid
-let scale = 1;
-
+const scale = 50;
+const xblock = 101;
+const yblock = 83;
+const yOffSet = -20;
+const speedOffSet = 200;
+let GAME_OVER = false;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -8,8 +17,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 100;
-    this.y = 100
+    this.x = getRandomInt(-500,-100);//delay bugs to show on canvas
+    this.y = yOffSet + getRandomInt(1,4) * yblock;
+    this.speed = speedOffSet + getRandomInt(1,4) * scale;
+
 };
 
 // Update the enemy's position, required method for game
@@ -18,14 +29,32 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    // if (this.loc)
-    // this.loc += dt * scale;
+    if (!GAME_OVER){
+        console.log(GAME_OVER)
+        if (this.x < ctx.canvas.width){
+            this.x += this.speed * dt;
+            this.checkCollision(player);
+        }else{
+            this.x = getRandomInt(-500,-100);
+            this.y = yOffSet + getRandomInt(1,4) * yblock;
+            this.speed = speedOffSet + getRandomInt(1,4) * scale;
+        }
+    }
+
 
 };
+Enemy.prototype.checkCollision = function(player){
+    if(this.y === player.y){
+        if (Math.abs(this.x-player.x) <= 50){
+            GAME_OVER = true;
+        }
+    }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // console.log(this.x,this.y)
 };
 
 // Now write your own player class
@@ -34,23 +63,37 @@ Enemy.prototype.render = function() {
 
 var Player = function(){
     this.sprite = 'images/char-princess-girl.png';
-    this.x = 200;
-    this.y = 200;
+
+    //initial coordinate (2,4);
+    this.x = 2 * xblock;
+    this.y = yOffSet + 4 * yblock;
 }
-Player.prototype.update = function(dt){
+Player.prototype.update = function(x = 0, y = 0){
+    this.x += x;
+    this.y += y;
 
 }
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-Player.prototype.handleInput = function(keys){
-
+Player.prototype.handleInput = function(key){
+    if (!GAME_OVER){
+        if (key === 'up'){
+            this.update(0,-yblock);
+        }else if (key === 'down'){
+            this.update(0,yblock);
+        }else if (key === 'left'){
+            this.update(-xblock,0);
+        }else if (key === 'right'){
+            this.update(xblock,0);
+        }
+    }
 }
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [new Enemy()]
+var allEnemies = [new Enemy(),new Enemy(),new Enemy(),new Enemy()]
 var player = new Player()
 
 // This listens for key presses and sends the keys to your
